d03c3275ea0478e321eecc0254f38e14f6039387
LouisDeMilleville <125300400+LouisDeMilleville@users.noreply.github.com>
2023-11-30 17:50:32 -0500

    Ajout du fichier FonctionsOnClick.js qui contient les différentes fonctions exécutées lorsqu'une div est cliquée (pas encore mis de code dedans).

---
+++ js/FonctionsOnClick.js
@@ -0,0 +1,107 @@
+function enableOnClick()
+{
+    let InfluenceBeneGeserit1 = window.document.getElementById("influence_bene_geserit_1");
+    let InfluenceBeneGeserit2 = window.document.getElementById("influence_bene_geserit_2");
+
+    let InfluenceTheEmperor1 = window.document.getElementById("influence_the_emperor_1");
+    let InfluenceTheEmperor2 = window.document.getElementById("influence_the_emperor_2");
+
+    let InfluenceSpacingGuild1 = window.document.getElementById("influence_spacing_guild_1");
+    let InfluenceSpacingGuild2 = window.document.getElementById("influence_spacing_guild_2");
+
+    let InfluenceFremen1 = window.document.getElementById("influence_fremen_1");
+    let InfluenceFremen2 = window.document.getElementById("influence_fremen_2");
+
+    let Reserve = window.document.getElementById("reserve");
+
+    let ImperiumDeck0 = window.document.getElementById("imperium_deck_0");
+    let ImperiumDeck1 = window.document.getElementById("imperium_deck_1");
+    let ImperiumDeck2 = window.document.getElementById("imperium_deck_2");
+
+    let ImperiumRow1 = window.document.getElementById("imperium_row_1");
+    let ImperiumRow2 = window.document.getElementById("imperium_row_2");
+    let ImperiumRow3 = window.document.getElementById("imperium_row_3");
+
+    let Intrigue1 = window.document.getElementById("intrigue_1");
+    let Intrigue2 = window.document.getElementById("intrigue_2");
+
+    let Conflict1 = window.document.getElementById("conflict_1");
+    let Conflict2 = window.document.getElementById("conflict_2");
+
+
+    InfluenceBeneGeserit1.addEventListener("click", function () {
+        //Code
+    });
+
+    InfluenceBeneGeserit2.addEventListener("click", function () {
+        //Code
+    });
+
+    InfluenceTheEmperor1.addEventListener("click", function () {
+        //Code
+    });
+
+    InfluenceTheEmperor2.addEventListener("click", function () {
+        //Code
+    });
+
+    InfluenceSpacingGuild1.addEventListener("click", function () {
+        //Code
+    });
+
+    InfluenceSpacingGuild2.addEventListener("click", function () {
+        //Code
+    });
+
+    InfluenceFremen1.addEventListener("click", function () {
+        //Code
+    });
+
+    InfluenceFremen2.addEventListener("click", function () {
+        //Code
+    });
+
+    Reserve.addEventListener("click", function () {
+        //Code
+    });
+
+    ImperiumDeck0.addEventListener("click", function () {
+        //Code
+    });
+
+    ImperiumDeck1.addEventListener("click", function () {
+        //Code
+    });
+
+    ImperiumDeck2.addEventListener("click", function () {
+        //Code
+    });
+
+    ImperiumRow1.addEventListener("click", function () {
+        //Code
+    });
+
+    ImperiumRow2.addEventListener("click", function () {
+        //Code
+    });
+
+    ImperiumRow3.addEventListener("click", function () {
+        //Code
+    });
+
+    Intrigue1.addEventListener("click", function () {
+        //Code
+    });
+
+    Intrigue2.addEventListener("click", function () {
+        //Code
+    });
+
+    Conflict1.addEventListener("click", function () {
+        //Code
+    });
+
+    Conflict2.addEventListener("click", function () {
+        //Code
+    });
+}
